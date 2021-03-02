394c9e7f85f71612a94ae46503e2529cf4cf3908
Sayana <sayana_0308@mail.ru>
2021-03-02 13:58:35 +0900

    first commit

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,23 +12,23 @@ class MainController extends AppController
 {
     public function indexAction()
     {
-//        debug($this->route);
-//        debug($this->controller);
-//        echo __METHOD__;
-        $posts = R::findAll('test');
-//       debug($posts);
-
-        $name = 'Sayana';
-        $age = 37;
-        $names = ['Roman', 'Artur', 'Aldar'];
-        $cache = Cache::instance();
-//        $cache->set('test', $names);
-        $cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data) {
-            $cache->set('test', $names);
-        }
-//        debug($data);
+////        debug($this->route);
+////        debug($this->controller);
+////        echo __METHOD__;
+//        $posts = R::findAll('test');
+////       debug($posts);
+//
+//        $name = 'Sayana';
+//        $age = 37;
+//        $names = ['Roman', 'Artur', 'Aldar'];
+//        $cache = Cache::instance();
+////        $cache->set('test', $names);
+//        $cache->delete('test');
+//        $data = $cache->get('test');
+//        if (!$data) {
+//            $cache->set('test', $names);
+//        }
+////        debug($data);
         $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
--- app/views/Main/index.php
+++
@@ -1,8 +0,0 @@
-<h1>Hello world</h1>
-
-<?=$name;?>
-<?=$age;?>
-
-<?php foreach ($posts as $post): ?>
-    <h3><?=$post->title; ?></h3>
-<?php endforeach; ?>
